5bf44122d864f5a26ee8e45ed128c422a3942d8c
reemharbi <reemharbi.3@gmail.com>
2022-01-17 16:55:28 +0300

    added cheatsheets

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,17 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
     {
       resolve: "smooth-doc",
       options: {
-        name: "Smooth DOC Starter",
+        name: "Rem || Smooth Doc",
         description: "Rem Documentation Using Smooth Doc",
         siteUrl: "https://remdoc.gatsbyjs.io/",
+        baseDirectory: path.resolve(__dirname, '../'),
+        author: 'Rem',
+        sections: ['About', 'Cheatsheet', 'Components', 'Reference'],
+        navItems: [{ title: 'Docs', url: '/docs/' }],
       },
     },
   ],
